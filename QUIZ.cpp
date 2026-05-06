d123c0678ec212185e3361cc5c272d391353ad65
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-06 11:39:52 -0500

    commit finalizacion del programa

--- QUIZ.cpp
+++ QUIZ.cpp
@@ -7,8 +7,6 @@ class Product{
     int price;
     int amount;
     
-    protected:
-    int total;
 
     public:
     void setName(std::string n){
@@ -29,34 +27,48 @@ class Product{
     int getAmount(){
         return amount;
     } 
-    int total(int c, int d){
-        return c*d;
-    }
+    
 };
 
 class Discount : public Product{
     
     private:
-    int discount;
+    float discount;
     
     public:
-    int price_disc;
-    void setDiscount(int des){
+    float price_disc;
+    void setDiscount(float des){
         this->discount= des;
     }
-    int getDiscount(){
+    float getDiscount(){
         return discount;
     }
-    int price_disc(int total, int discount){
-        return total*discount;
-    }
+   
 };
 
 int main(){
     
     Product product1;
     Discount product_desc;
+    int total;
+    float desc;
     product1.setName("Iphone");
     product1.getName();
-    std::cout << "Nombre: " <<
+    product1.setPrice(2800000);
+    product1.getPrice();
+    product1.setAmount(1);
+    product1.getAmount();
+ total= product1.getPrice() *  product1.getAmount();
+    std::cout << "Nombre: " << product1.getName() << std::endl;
+    std::cout << "Precio: " << product1.getPrice() << std::endl;
+    std::cout << "Cantidad: " << product1.getAmount() << std::endl;
+    std::cout << "Total sin descuento: " << total << std::endl;
+
+    product_desc.setDiscount(0.20);
+    product_desc.getDiscount();
+
+    desc= total* product_desc.getDiscount();
+    std::cout << "Precio con descuento: " << desc << std::endl;
+
+    return 0;
 }
